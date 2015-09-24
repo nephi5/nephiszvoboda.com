77299b64ea57bedf2f054d76d4d7dfedeb024944
Nephi Szvoboda <nephi.szvoboda@gmail.com>
2015-09-24 19:19:51 +0200

    added basic skeleton to index.php

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<!-- Home page of nephiszvoboda.com -->
+<html lang="en-us">
+    <head>
+        <meta charset="utf-8">
+        <meta name="author" content="Nephi Szvoboda">
+        <title>Home Page</title>
+    </head>
+    <body>
+        <div>
+            <h1>Home</h1>
+            <p>
+                Nephi Szvoboda is a Fullstack Web Developer, the purpose of this site is to show off his work and projects.
+            </p>
+            <header role="banner">
+                <div>
+                </div>
+            </header>
+            <nav role="navigation">
+                <div>
+                </div>
+            </nav>
+            <main role="main">
+                <div>
+                </div>
+            </main>
+            <aside role="complementary">
+                <div>
+                </div>
+            </aside>
+            <footer role="contentinfo">
+                <div>
+                    <ul>
+                        <li>Site Plan</li>
+                        <li>Teaching Presentation</li>
+                    </ul>
+                </div>
+            </footer>
+        </div>
+
+    </body>
+</html>
